0cdf44fe57e02827d30e2ff4e11e19cfc4247e44
Lexuspr <lexuspr@hotmail.com>
2019-03-01 09:34:24 -0500

    Primer commit

---
+++ precache-manifest.1359e185deb3c70a5952d9c04a2dc0f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2eb4d4958c100f84fcda",
+    "url": "/static/css/main.89675109.chunk.css"
+  },
+  {
+    "revision": "2eb4d4958c100f84fcda",
+    "url": "/static/js/main.2eb4d495.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "f167ac13f4594f975baaa823ca565cff",
+    "url": "/index.html"
+  }
+];
